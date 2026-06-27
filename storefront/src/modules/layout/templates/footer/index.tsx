469877b67cce3c42211b8e43d414f2775407a984
Nitesh <niteshspp189>
2026-06-27 18:28:02 +0530

    fix: remove 'Our Brands' link from footer section as requested in client mail point 11

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -41,7 +41,6 @@ export default async function Footer() {
             <h4 style={s.colTitle as any} className="text-gray-200">ABOUT PRO CARE</h4>
             <LocalizedClientLink href="/our-story" style={s.link} className="hover:text-white transition-colors">Our Story</LocalizedClientLink>
             <a href="https://mvshoecare.com/careers" target="_blank" rel="noopener noreferrer" style={s.link} className="hover:text-white transition-colors">Career</a>
-            <LocalizedClientLink href="/" style={s.link} className="hover:text-white transition-colors">Our Brands</LocalizedClientLink>
             <LocalizedClientLink href="/" style={s.link} className="hover:text-white transition-colors">Responsibility</LocalizedClientLink>
             <LocalizedClientLink href="/" style={s.link} className="hover:text-white transition-colors">Quality</LocalizedClientLink>
           </div>
