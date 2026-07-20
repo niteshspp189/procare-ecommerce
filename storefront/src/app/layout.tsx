2fb36540eb86826c12988974298d538731c28cca
Nitesh <niteshspp189>
2026-07-20 16:31:08 +0530

    fix: load GTM script beforeInteractive so script renders in SSR head HTML

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         {/* Google Tag Manager */}
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
