440b6e296d8f392e5a445974059ff331e028f24a
Nitesh <niteshspp189>
2026-08-21 17:01:18 +0530

    chore: remove root package.json and backend-static

--- .tempmediaStorage/scripts/check_pending.py
+++
@@ -1,6 +0,0 @@
-import os
-pending_dir = "/mnt/ExtraStorage/Project-Files/session-2026/procare/ecomm/procare-mail/july7/unzipped/Pending"
-for root, dirs, files in os.walk(pending_dir):
-    for f in files:
-        if f.lower().endswith(('.jpg', '.jpeg', '.png', '.cr3')):
-            print(os.path.join(root, f))
